dad563801223bc62cfac16cd9d8483190d1de711
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2024-05-24 00:43:17 +0200

    try_auth

--- template.rb
+++ template.rb
@@ -87,12 +87,12 @@ def install_devise
   generate 'devise User'
 end
 
-def create_session_controller
+def create_omniauth_session_controller
   # generate omniauth controller
-  generate :controller, 'Omniaut create destroy'
+  generate :controller, 'OmniauthSessions create destroy'
 
   # insert omniauth controller code
-  insert_into_file 'app/controllers/omniauth_controller.rb', after: "def create\n" do
+  insert_into_file 'app/controllers/omniauth_sessions_controller.rb', after: "def create\n" do
     <<-CODE
       auth = request.env['omniauth.auth']
       user = User.find_by(provider: auth['provider'], uid: auth['uid']) || User.create_with_omniauth(auth)
@@ -101,7 +101,7 @@ def create_session_controller
     CODE
   end
 
-  insert_into_file 'app/controllers/omniauth_controller.rb', after: "def destroy\n" do
+  insert_into_file 'app/controllers/omniauth_sessions_controller.rb', after: "def destroy\n" do
     <<-CODE
       sign_out
       redirect_to root_url, notice: 'Signed out!'
@@ -126,6 +126,56 @@ def add_omniauth_to_user_model
   end
 end
 
+def init_docker_compose
+  file 'docker-compose.yml', <<-CODE
+    version: '3.3'
+    services:
+      postgres:
+        image: postgres:14
+        container_name: postgres_#{@app_name}
+        ports:
+        - '127.0.0.1:5432:5432'
+        environment:
+          POSTGRES_USER: postgres
+          POSTGRES_PASSWORD: postgres
+          POSTGRES_HOST_AUTH_METHOD: "trust"
+        volumes:
+          - ${PWD}/db:${PWD}/db
+      redis:
+        image: redis:7
+        ports:
+          - '127.0.0.1:6379:6379'
+        container_name: redis_#{@app_name}
+  CODE
+end
+
+def configure_database
+  # configure database
+  environment "config.active_record.schema_format = :sql", env: 'development'
+
+  # write database.yml
+  file 'config/database.yml', <<-CODE
+    default: &default
+      adapter: postgresql
+      encoding: unicode
+      host: <%= ENV.fetch('POSTGRES_HOST', 'localhost') %>
+      port: <%= ENV.fetch('POSTGRES_PORT', 5432) %>
+      pool: <%= ENV.fetch('RAILS_MAX_THREADS', 5) %>
+      username: postgres
+      password: postgres
+
+    development:
+      <<: *default
+      database: #{@app_name}_development
+
+    test:
+      <<: *default
+      database: #{@app_name}_test
+
+    production:
+      url: <%= ENV['DATABASE_URL'] %>
+  CODE
+end
 
 add_gems
 
@@ -142,107 +192,22 @@ generate :model, 'Identity user:references provider:string uid:string'
 
 generate :controller, 'home index'
 
+insert_into_file 'app/views/home/index.html.slim' do
+  <<-CODE
+  h2.block-title Sign in with
+  ul.nav.nav-pills
+    li.nav-item= link_to '/auth/developer', 'Sign in with Developer'
+    li.nav-item= link_to '/users/sign_in', 'Sign in with Email'
+  CODE
+end
+
+
 route "root to: 'home#index'"
 
-create_session_controller
+create_omniauth_session_controller
 
 add_omniauth_to_user_model
 
-# # configure database
-# environment "config.active_record.schema_format = :sql", env: 'development'
+init_docker_compose
 
-# # write database.yml
-# file 'config/database.yml', <<-CODE
-# default: &default
-#   adapter: postgresql
-#   encoding: unicode
-#   host: <%= ENV.fetch('POSTGRES_HOST', 'localhost') %>
-#   port: <%= ENV.fetch('POSTGRES_PORT', 5432) %>
-#   pool: <%= ENV.fetch('RAILS_MAX_THREADS', 5) %>
-#   username: postgres
-#   password: postgres
-
-# development:
-#   <<: *default
-#   database: #{@app_name}_development
-
-# test:
-#   <<: *default
-#   database: #{@app_name}_test
-
-# production:
-#   url: <%= ENV['DATABASE_URL'] %>
-# CODE
-
-# # add omniauth gem
-# gem 'omniauth'
-
-# installed_auth_providers = []
-
-# # ask if we want to install any provider
-# if yes?("Do you want to install any omnia
-# file 'app/controllers/sessions_controller.rb', <<-CODE
-# class SessionsController < ApplicationController
-#   def create
-#     auth = request.env['omniauth.auth']
-#     user = User.find_by(provider: auth['provider'], uid: auth['uid']) || User.create_with_omniauth(auth)
-#     session[:user_id] = user.id
-#     redirect_to root_url, notice: 'Signed in!'
-#   end
-
-#   def destroy
-#     session[:user_id] = nil
-#     redirect_to root_url, notice: 'Signed out!'
-#   end
-# end
-# CODE
-
-# file 'app/models/user.rb', <<-CODE
-# class User < ApplicationRecord
-#   def self.create_with_omniauth(auth)
-#     create! do |user|
-#       identity = Identity.create!(provider: auth['provider'], uid: auth['uid'])
-#       user.identities << identity
-#       user.first_name = auth['info']['first_name']
-#       user.last_name = auth['info']['last_name']
-#       user.email = auth['info']['email']
-#     end
-#   end
-# end
-# CODE
-
-# # add sessions views
-# file 'app/views/sessions/create.html.erb', <<-CODE
-# <h1>Successfully signed in!</h1>
-# CODE
-
-# file 'app/views/sessions/destroy.html.erb', <<-CODE
-# <h1>Successfully signed out!</h1>
-# CODE
-
-
-# # configure database
-# environment "config.active_record.schema_format = :sql", env: 'development'
-
-# # write database.yml
-# file 'config/database.yml', <<-CODE
-# default: &default
-#   adapter: postgresql
-#   encoding: unicode
-#   host: <%= ENV.fetch('POSTGRES_HOST', 'localhost') %>
-#   port: <%= ENV.fetch('POSTGRES_PORT', 5432) %>
-#   pool: <%= ENV.fetch('RAILS_MAX_THREADS', 5) %>
-#   username: postgres
-#   password: postgres
-
-# development:
-#   <<: *default
-#   database: #{@app_name}_development
-
-# test:
-#   <<: *default
-#   database: #{@app_name}_test
-
-# production:
-#   url: <%= ENV['DATABASE_URL'] %>
-# CODE
+configure_database
